2afb093b981a66a497035186c4cff4dae1857072
dayfan0810 <mr_day@163.com>
2023-12-15 00:38:06 +0800

    111

--- ProxyMode/src/main/java/org/example/algo/$1_sort/BubbleSortAlgorithm.java
+++ ProxyMode/src/main/java/org/example/algo/$1_sort/BubbleSortAlgorithm.java
@@ -1,7 +1,5 @@
 package org.example.algo.$1_sort;
 
-import org.example.algo.SortAlgo;
-
 public class BubbleSortAlgorithm extends SortAlgo {
     public static void main(String[] args) {
         int[] arr = {11, 5, 88, 13, 12, 18, 11, 0, 89}; // 将此数组按照从小到大顺序排列
--- ProxyMode/src/main/java/org/example/algo/$1_sort/SortAlgo.java
+++ ProxyMode/src/main/java/org/example/algo/$1_sort/SortAlgo.java
@@ -1,4 +1,4 @@
-package org.example.algo;
+package org.example.algo.$1_sort;
 
 public class SortAlgo {
 
